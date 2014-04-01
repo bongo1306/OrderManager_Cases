a03a22bde7b99923c69391f0d0836b286983ea12
Travis Stuart <Travis.Stuart@Heatcraftrpd.com>
2014-04-01 15:50:19 -0400

    LabelCtrlDbLinker now works as expected.

--- LabelCtrlDbLinker.py
+++ LabelCtrlDbLinker.py
@@ -10,7 +10,6 @@ class LabelCtrlDbLinker(wx.StaticText):
 		pre = wx.PreStaticText()
 		self.PostCreate(pre)
 		self.Bind(wx.EVT_WINDOW_CREATE, self.on_create)
-		#self.Bind(wx.EVT_KILL_FOCUS, self.on_focus_lost)
 		self.Bind(wx.EVT_LEFT_DOWN, self.on_left_down)
 
 
@@ -23,28 +22,6 @@ class LabelCtrlDbLinker(wx.StaticText):
 
 	def on_left_down(self, event):
 		LabelEditFrame(self)
-
-
-	"""
-	def on_focus_lost(self, event):
-		#parse our the table name and field from the control name
-		table = '.'.join(self.GetName().split(':')[1].split('.')[0:2])
-		table_id = wx.GetTopLevelParent(self).id
-		
-		field = self.GetName().split(':')[1].split('.')[2]
-		new_value = self.GetStringSelection()
-		
-		db.update_order(table, table_id, field, new_value)
-
-		parent_frame = wx.GetTopLevelParent(self)
-		parent_frame.Freeze()
-		parent_frame.reset_all()
-		parent_frame.populate_all()
-		parent_frame.Thaw()
-
-		
-		event.Skip()
-	"""
 
 
 
@@ -60,7 +37,7 @@ class LabelEditFrame(wx.Frame):
 		self.panel = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
 		sizer2 = wx.BoxSizer(wx.HORIZONTAL)
 		
-		self.text = wx.TextCtrl(self.panel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(150,-1), 0)
+		self.text = wx.TextCtrl(self.panel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(200,-1), wx.TE_PROCESS_ENTER)
 		sizer2.Add(self.text, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 0)
 		
 		self.panel.SetSizer(sizer2)
@@ -82,11 +59,17 @@ class LabelEditFrame(wx.Frame):
 		
 		self.text.SetValue(current_value)
 		
+		self.Bind(wx.EVT_TEXT_ENTER, self.on_text_enter)
 		self.text.Bind(wx.EVT_KILL_FOCUS, self.on_focus_lost)
 		self.Bind(wx.EVT_CLOSE, self.on_close_frame)
 
 		self.Move((parent.GetScreenPosition()))
 		self.Show()
+
+
+	def on_text_enter(self, event):
+		self.parent.SetFocus()
+		#this will trigger the frame to close
 
 
 	def on_focus_lost(self, event):
@@ -95,6 +78,21 @@ class LabelEditFrame(wx.Frame):
 
 
 	def on_close_frame(self, event):
+		#parse our the table name and field from the control name
+		table = '.'.join(self.parent.GetName().split(':')[1].split('.')[0:2])
+		table_id = wx.GetTopLevelParent(self.parent).id
+		
+		field = self.parent.GetName().split(':')[1].split('.')[2]
+		new_value = self.text.GetValue()
+		
+		db.update_order(table, table_id, field, new_value)
+
+		parent_frame = wx.GetTopLevelParent(self.parent)
+		parent_frame.Freeze()
+		parent_frame.reset_all()
+		parent_frame.populate_all()
+		parent_frame.Thaw()
+		
 		self.Destroy()
 
 
