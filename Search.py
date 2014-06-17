06b437a6868dcd14399917c01482a0a345680124
Travis Stuart <Travis.Stuart@Heatcraftrpd.com>
2014-06-17 10:17:50 -0400

    Added dbo.mmg_processed table to search choices.

--- Search.py
+++ Search.py
@@ -29,7 +29,7 @@ class SearchTab(object):
 		
 		#tables or views the user can search in
 		#tables = ('orders.root', 'orders.view_systems', 'orders.view_systems_abridged', 'orders.responsibilities', 'orders.target_dates', 'orders.labor_hours', 'orders.financials', 'orders.misc', 'orders.changes', 'orders.time_logs', 'dbo.orders', 'dbo.mmg_uploads')
-		tables = ('orders.root', 'orders.view_systems', 'orders.view_systems_abridged', 'orders.changes', 'orders.time_logs', '-------------------------', 'dbo.mmg_uploads', 'dbo.orders', 'dbo.ecrs', 'dbo.revisions', '-------------------------', 'tss.fsrs', 'tss.rsrs', '-------------------------', 'dbo.projects', 'dbo.tasks')
+		tables = ('orders.root', 'orders.view_systems', 'orders.view_systems_abridged', 'orders.changes', 'orders.time_logs', '-------------------------', 'dbo.mmg_uploads', 'dbo.mmg_processed', '-------------------------', 'dbo.orders', 'dbo.ecrs', 'dbo.revisions', '-------------------------', 'tss.fsrs', 'tss.rsrs', '-------------------------', 'dbo.projects', 'dbo.tasks')
 		
 		ctrl(self, 'choice:which_table').AppendItems(tables)
 		ctrl(self, 'choice:which_table').SetStringSelection('orders.view_systems_abridged')
