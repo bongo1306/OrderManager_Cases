b2a657c04f39eb7d9c742e269c308ee8e6d809c2
Travis Stuart <Travis.Stuart@Heatcraftrpd.com>
2014-06-30 15:23:00 -0400

    Made the new mmg tables search results openable.

--- Search.py
+++ Search.py
@@ -162,6 +162,40 @@ class SearchTab(object):
 			else:
 				wx.MessageBox('Production Order was not found in the database.', 'Order Not Found', wx.OK | wx.ICON_ERROR)
 
+		if table_name == 'mmg.change_requests':
+			sap_prod_ord = selected_item.GetItem(selected_item.GetFirstSelected(), 2).GetText()
+
+			table_id = db.query("SELECT TOP 1 id FROM orders.root WHERE production_order = '{}'".format(sap_prod_ord))
+
+			if table_id:
+				Item.ItemFrame(self, int(table_id[0]))
+				
+			else:
+				wx.MessageBox('Production Order was not found in the database.', 'Order Not Found', wx.OK | wx.ICON_ERROR)
+
+		if table_name == 'mmg.processed':
+			sap_prod_ord = selected_item.GetItem(selected_item.GetFirstSelected(), 2).GetText()
+
+			table_id = db.query("SELECT TOP 1 id FROM orders.root WHERE production_order = '{}'".format(sap_prod_ord))
+
+			if table_id:
+				Item.ItemFrame(self, int(table_id[0]))
+				
+			else:
+				wx.MessageBox('Production Order was not found in the database.', 'Order Not Found', wx.OK | wx.ICON_ERROR)
+
+		if table_name == 'mmg.view_processed_ecrs':
+			sap_prod_ord = selected_item.GetItem(selected_item.GetFirstSelected(), 3).GetText()
+
+			table_id = db.query("SELECT TOP 1 id FROM orders.root WHERE production_order = '{}'".format(sap_prod_ord))
+
+			if table_id:
+				Item.ItemFrame(self, int(table_id[0]))
+				
+			else:
+				wx.MessageBox('Production Order was not found in the database.', 'Order Not Found', wx.OK | wx.ICON_ERROR)
+
+
 
 
 	def on_choice_table(self, event=None):
